ad3f0b8b02a472b1f37bb54deca7d68ca5ab4176
Brock Taban <hello@brocktaban.com>
2019-05-02 03:00:08 -0400

    Added Routing Edited RouterLinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,28 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CommandsComponent } from './pages/commands/commands.component';
+import { GamesComponent } from './pages/games/games.component';
+import { GamesInfoComponent } from './pages/games-info/games-info.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { AccountComponent } from './pages/account/account.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { AdminStreamersComponent } from './pages/admin-streamers/admin-streamers.component';
+import { AdminCommandsComponent } from './pages/admin-commands/admin-commands.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent,
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: HomeComponent,
     children: [
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      { path: 'dashboard', component: DashboardComponent }
+      { path: '', component: DashboardComponent },
+      { path: 'commands', component: CommandsComponent },
+      { path: 'games', component: GamesComponent },
+      { path: 'games/:id', component: GamesInfoComponent },
+      { path: 'settings', component: SettingsComponent },
+      { path: 'account', component: AccountComponent },
+      { path: 'admin/dashboard', component: AdminComponent },
+      { path: 'admin/users', component: AdminStreamersComponent },
+      { path: 'admin/commands', component: AdminCommandsComponent },
     ]
   }
 ];
